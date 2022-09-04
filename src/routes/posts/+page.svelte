6292d8976ee1f2c869a9369007ad33d4dc367f17
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-09-05 00:35:12 +0300

    5

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -3,14 +3,17 @@
     import { database } from "../../firebase/firebase.js";
     import { ref, child, set, get, push, update, onValue } from "firebase/database";
     import { onMount } from "svelte";
+    import { assign } from "svelte/internal";
 
-    let post = { title: "", text: "", image: null };
+    let post = { key: "", value: { title: "", text: "", image: null } };
+    let mapPosts = new Map();
     let posts = [];
 
     onMount(() => {
         onValue(ref(database, "posts"), snap => {
+            mapPosts = snap.val();
             posts = Object.values(snap.val());
-            console.log(posts);
+            console.log(post);
         });
     });
 </script>
@@ -19,30 +22,35 @@
     <h5 class="mb-3 ps-1">Добавить новую публикацию</h5>
     <div class="row">
         <div class="col">
-            <input bind:value={post.title} class="form-control mb-3" placeholder="Заголовок" />
-            <input bind:value={post.text} class="form-control mb-3" placeholder="Текст" />
-            <input bind:value={post.image} class="form-control mb-3" placeholder="Адрес картинки" />
+            <input bind:value={post.value.title} class="form-control mb-3" placeholder="Заголовок" />
+            <input bind:value={post.value.text} class="form-control mb-3" placeholder="Текст" />
+            <input bind:value={post.value.image} class="form-control mb-3" placeholder="Адрес картинки" />
             <button
                 class="btn btn-dark"
                 on:click={() => {
-                    const key = push(child(ref(database), "posts")).key;
-                    update(ref(database, `posts/ ${key}`), post);
-                    post = { title: "", text: "", image: null };
+                    console.log(post.key);
+                    if (post.key === "") {
+                        const key = push(child(ref(database), "posts")).key;
+                        update(ref(database, `posts/${key}`), post.value);
+                    } else {
+                        update(ref(database, `posts/${post.key}`), post.value);
+                    }
+                    post = { key: "", value: { title: "", text: "", image: null } };
                 }}>Сохранить</button
             >
             <button
                 class="btn btn-light"
                 on:click={() => {
-                    post = { title: "", text: "", image: null };
+                    post = { key: "", value: { title: "", text: "", image: null } };
                 }}>Отмена</button
             >
         </div>
-        <div class="col-2"><img class="img-fluid rounded" src={post.image} alt="" /></div>
+        <div class="col-2"><img class="img-fluid rounded" src={post.value.image} alt="" /></div>
     </div>
 </div>
 
 <div class="row row-cols-1 row-cols-md-4 g-4">
-    {#each posts as item}
+    {#each Object.entries(mapPosts) as [key, item]}
         <div class="col">
             <div class="card h-100">
                 <div class="card-body">
@@ -51,7 +59,7 @@
                         <button
                             class="btn btn-sm bg-white"
                             on:click={() => {
-                                post = item;
+                                post = { key: key, value: item };
                             }}><i class="fa-solid fa-pen-to-square text-warning" /></button
                         >
                     </div>
